9198855894cf6a7b2820be9d1968e109bbb29929
arpan794 <arpan794@gmail.com>
2026-05-11 17:47:43 +0530

    update

---
+++ python_coding/minion_game.py
@@ -0,0 +1,28 @@
+def minion_game(string):
+    string = string.upper()
+
+    vowels = "AEIOU"
+
+    kevin_score = 0
+    stuart_score = 0
+    n = len(string)
+
+    for i, ch in enumerate(string):
+
+        if ch in vowels:
+            kevin_score += n - i
+        else:
+            stuart_score += n - i
+
+    if kevin_score > stuart_score:
+        print("Kevin", kevin_score)
+
+    elif stuart_score > kevin_score:
+        print("Stuart", stuart_score)
+
+    else:
+        print("Draw")
+    
+
+string = "banana"
+minion_game(string)
---
+++ python_coding/word_order.py
@@ -0,0 +1,27 @@
+# Input Format
+
+# The first line contains the integer, n .
+# The next n lines each contain a word.
+
+# Output Format
+
+# Output 2 lines.
+# On the first line, output the number of distinct words from the input.
+# On the second line, output the number of occurrences for each distinct word according to their appearance in the input.
+
+# Sample Input
+
+# 4
+# bcdef
+# abcdefg
+# bcde
+# bcdef
+# Sample Output
+
+# 3
+# 2 1 1
+# Explanation
+
+# There are 3 distinct words. Here, "bcdef" appears twice in the input at the first and last positions. The other words appear once each. The order of the first appearances are "bcdef", "abcdefg" and "bcde" which corresponds to the output.
+
+
